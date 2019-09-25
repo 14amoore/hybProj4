8e3ddfc26387628012ccbd992537f94043f9ca06
Adam Moore <adamoore@gmail.com>
2019-09-25 12:35:34 -0400

    maybe this works?

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,10 @@
 let synth = new Tone.Synth().toMaster();
-
 window.onload = function firstTone() {
   synth.triggerAttackRelease('C4', '8n');
 };
 
 document.querySelector('#play').addEventListener('click', () => Tone.start());
 
-document.querySelector('#tone').onmouseover = function() {
-  synth.triggerAttackRelease('B4', '8n', 0);
+document.querySelector('#c').onmouseover = function() {
+  synth.triggerAttackRelease('C3', '8n');
 };
